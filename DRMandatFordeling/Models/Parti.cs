bab8428062af81531af594b777958afb683a9971
William Kruse <williamkruse4@gmail.com>
2023-04-10 13:19:39 +0200

    ny klasse tilfoejet i stedet for record

---
+++ DRMandatFordeling/Models/Parti.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRMandatFordeling.Models
+{
+    internal class Parti
+    {
+        public string Navn { get; set; }
+        public int Stemmer { get; set; }
+        public int Mandater { get; set; } = 0;
+        
+    public Parti(string navn, int stemmer)
+        {
+            this.Navn = navn;
+            this.Stemmer = stemmer;
+        }
+    }
+}
--- DRMandatFordeling/Program.cs
+++ DRMandatFordeling/Program.cs
@@ -1,15 +1,22 @@
-﻿
-List<Parti> OpstilledePartier = new List<Parti>(new Parti("A", 405), new Parti("B",925), new Parti("C", 612), new Parti("D", 310));
+﻿using DRMandatFordeling.Models;
+
+List<Parti> OpstilledePartier = new List<Parti>()
+{
+	new Parti("A", 405),
+	new Parti("B",925),
+	new Parti("C", 612),
+	new Parti("D", 310)
+};
 
 List<Parti> ValgResultat = FordelMandater(OpstilledePartier, 5);
 
 for (int i = 0; i < ValgResultat.Count; i++)
 {
-	Console.WriteLine(ValgResultat[i].Navn +" har fået: " + ValgResultat[i].Stemmer + " og dermed opnået: " + ValgResultat[i].mandater);
+	Console.WriteLine(ValgResultat[i].Navn + " har fået: " + ValgResultat[i].Stemmer + " og dermed opnået: " + ValgResultat[i].Mandater); ;
 }
 
 
-List<Parti> FordelMandter(List<Parti> StemmeData, int AntalMandaterTilFordeling)
+List<Parti> FordelMandater(List<Parti> StemmeData, int AntalMandaterTilFordeling)
 {
 	List<Parti> Resultat = StemmeData;
 
@@ -24,15 +31,15 @@ List<Parti> FordelMandter(List<Parti> StemmeData, int AntalMandaterTilFordeling)
     for (int i = 0; i < AntalMandaterTilFordeling; i++)
 	{
 		int Index = 0;
-		int Kvotient = 0;
+		double Kvotient = 0;
 		
-		for (int i = 0; i < antalPartier; i++)
+		for (int j = 0; j < antalPartier; j++)
 		{
-			double tempKvotient = StemmeData[i].Stemmer / TempMandatFordeling[i] + 1;
+			double tempKvotient = StemmeData[j].Stemmer / (TempMandatFordeling[j] + 1);
 			if (tempKvotient>Kvotient)
 			{
 				Kvotient = tempKvotient;
-				Index = i;
+				Index = j;
 			}
 		}
 		TempMandatFordeling[Index]++;
@@ -43,4 +50,3 @@ List<Parti> FordelMandter(List<Parti> StemmeData, int AntalMandaterTilFordeling)
 
 
 
-new public record Parti (string Navn, int Stemmer, int Mandater = 0);
